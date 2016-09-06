0fea53701cda84e5ea479e143d24dbdb718ef796
Yu Wu <yuwu2@paypal.com>
2016-09-05 17:44:37 -0700

    feat(all) Finished initial npm related info

---
+++ spec/package-info-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PackageInfoView from '../lib/package-info-view';
+
+describe('PackageInfoView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
